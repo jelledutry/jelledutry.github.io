88b2baf897528b2a29776e6b13d7ff9716116509
jelldutr <jelle.dutry@student.arteveldehs.be>
2018-06-18 17:01:08 +0200

    semi-responsive

--- js/main.js
+++ js/main.js
@@ -3,16 +3,36 @@ window.onload = function() {
 // TAGLINE ANIMATIE
     var anim = lottie.loadAnimation({
         container: document.getElementById('tagline'), // the dom element that will contain the animation
-        renderer: 'svg',
+        renderer: 'canvas',
         loop: true,
         autoplay: true,
-        path: '/images/tagline.json' // the path to the animation json
+        path: '/images/tagline5.json' // the path to the animation json
     });
+    anim.setSubframe(false);
+    lottie.setQuality('low');
 
 // TAGLINE POSITIE
 
 var taglineDiv = document.getElementById('tagline');
 taglineDiv.style.top = (screen.height/2)- ((taglineDiv.offsetWidth/1920*1080)/2) + "px";
 
+// MENU POSITIE
+var menuDiv = document.getElementById("menu");
+var bgElem = document.getElementById('bg');
+var bodyWidth = 1136;
+if (bg.offsetWidth > bodyWidth){
+    menuDiv.style.right = (bg.offsetWidth-bodyWidth)/2 + "px";
+}
+else {
+    menuDiv.style.right = 0;
+}
+
+// CONTACT EMAIL NO SPAM
+
+var first = "jelle.dutry";
+var last = "gmail.com";
+var complete = '<a href="mailto:' + first + '@' + last + '">' + first + '@' + last + '</a>';
+var emailP = document.getElementById('emailP');
+emailP.insertAdjacentHTML('afterbegin', complete );
 
 }
