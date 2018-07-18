29d5b4151623b15a2bfb2d5b3b6f7331f957ba79
jelldutr <jelle.dutry@student.arteveldehs.be>
2018-07-18 14:25:48 +0200

    improvements

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ window.onload = function() {
         loop: true,
         autoplay: true,
         path: '/images/tagline5.json' // the path to the animation json
+        
     });
     anim.setSubframe(false);
     lottie.setQuality('low');
@@ -14,7 +15,9 @@ window.onload = function() {
 // TAGLINE POSITIE
 
 var taglineDiv = document.getElementById('tagline');
-taglineDiv.style.top = (screen.height/2)- ((taglineDiv.offsetWidth/1920*1080)/2) + "px";
+var whoAmIDiv = document.getElementById('whoami');
+anim.addEventListener ("DOMReady", taglineDiv.style.top = (screen.height/2) - ((taglineDiv.offsetWidth/1920*1080)/2) - whoAmIDiv.offsetHeight + "px");
+
 
 // MENU POSITIE
 var menuDiv = document.getElementById("menu");
@@ -31,8 +34,38 @@ else {
 
 var first = "jelle.dutry";
 var last = "gmail.com";
-var complete = '<a href="mailto:' + first + '@' + last + '">' + first + '@' + last + '</a>';
+var complete = first + '@' + last;
+var eLink = "<a href='mailto:" + complete + "'>" + complete + "</a>";
 var emailP = document.getElementById('emailP');
-emailP.insertAdjacentHTML('afterbegin', complete );
+var emailItem = document.getElementById('emailItem');
+emailP.setAttribute('action', 'https://formspree.io/'+ complete);
+emailItem.insertAdjacentHTML('afterbegin', eLink );
 
+}
+
+// Contactform spamcheck verborgen veld
+
+function validateForm() {
+    var sCheck = document.forms["contactForm"]["sCheck"].value;
+
+    if (sCheck != ""){ //checkt of verborgen veld is ingevuld
+        return false;
+    }
+
+}
+
+// Menu button functionality
+
+function menuClick() {
+    
+    var menuBtn = document.getElementById('menuBtn');
+    var menuItems = document.getElementById('menu-items');
+    menuBtn.style.visibility = "hidden";
+    menuItems.style.visibility = "visible";
+
+
+    setTimeout(function() {
+        menuBtn.style.visibility = "visible";
+        menuItems.style.visibility = "hidden";
+      }, 4000);
 }
